4bce8982ebb91040178a1deabe545279f8817f53
pranathi kopparapu <pranathikopparapu05@gmail.com>
2026-01-07 08:03:06 +0530

    Fix API baseURL to use /api prefix for production backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
   import axios from "axios";
 
   const API = axios.create({
-    baseURL: "https://foodiee-backend.onrender.com/"
+    baseURL: "https://foodiee-backend.onrender.com/api"
 
   });
 
