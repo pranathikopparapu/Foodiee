154797be0a72e6421cc4b4cdad4730c5da77d6c5
pranathikopparapu <pranathikopparapu05@gmail.com>
2026-04-15 17:29:38 +0530

    Fixed API baseURL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://foodiee-mfjc.onrender.com/api/foods",
+  baseURL: "https://foodiee-mfjc.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
