8b75c48c1bd537d966747ce1221bfb3f50068f9e
pranathikopparapu <pranathikopparapu05@gmail.com>
2026-04-15 18:44:04 +0530

    force fix api baseURL

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
   /* ================= FETCH FOODS ================= */
   const fetchFoods = async () => {
     try {
-      const res = await API.get("/foods");
+      const res = await API.get("/api/foods");
 
       console.log("API DATA:", res.data); // 🔥 debug
 
